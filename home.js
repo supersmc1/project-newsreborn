f3930ad07e90eff49d3478fa5b0a4b39ef294cfc
sunwanchung <sunwanchung@gmail.com>
2022-06-14 14:46:17 +0900

    다시수정

--- home.js
+++ home.js
@@ -1,5 +1,4 @@
 const icon = document.querySelector('.icon')
-const item1 = document.querySelector('item1')
 
 icon.addEventListener('click',function(){
   window.location = 'https://supersmc1.github.io/project-reborn/'
