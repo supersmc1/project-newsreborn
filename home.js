67962db7f57cb1f1038e46b92a28c0de8c91dbbd
sunwanchung <sunwanchung@gmail.com>
2022-06-14 14:35:37 +0900

    수정중

--- home.js
+++ home.js
@@ -3,3 +3,9 @@ const icon = document.querySelector('.icon')
 icon.addEventListener('click',function(){
   window.location = 'https://supersmc1.github.io/project-reborn/'
 })
+
+const item1 = document.querySelector('.item1')
+
+item1.addEventListener('click',function(){
+  window.location = 'https://supersmc1.github.io/project-News.reborn/'
+})
